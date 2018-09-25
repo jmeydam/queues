db3a44b84349c5b5e85790af50c7119b03fe238a
Jens Meydam <jmeydam@gmail.com>
2018-09-25 08:30:59 +0200

    Refactored, using constants.

--- various/mm1_queue.ino
+++ various/mm1_queue.ino
@@ -4,14 +4,46 @@
 #include <LedControl.h>
 
 /*
- * Pin 12: DataIn
- * Pin 11: LOAD(CS)
- * Pin 10: CLK
- * One MAX7219, 8x8 LEDs
+ * set up led matrix for visualization
  */
-LedControl lc = LedControl(12, 10, 11, 1);
 
-int array_size = 64;
+// pin 10: CLK
+const int CLK = 10;
+// pin 11: LOAD(CS)
+const int LOAD = 11;
+// pin 12: DataIn
+const int DATA_IN = 12;
+// one MAX7219, 8x8 LEDs
+const int MAX7219 = 1;
+
+LedControl lc = LedControl(DATA_IN, CLK, LOAD, MAX7219);
+
+/*
+ * parameters of simulation
+ */
+
+// seed for random number generator
+const int SEED = 1234;
+// enqueueing with probability ARRIVAL_PROB (in %)
+// try: 25.0, 30.0, 35.0
+const float ARRIVAL_PROB = 35.0;
+// dequeueing with probability DEPARTURE_PROB (in %)
+const float DEPARTURE_PROB = 30.0;
+// enable ('Y') or disable ('N') control
+const char CONTROL = 'Y';
+// check and truncate queue every QUEUE_CONTROL_INTERVAL iterations
+const int QUEUE_CONTROL_INTERVAL = 10;
+// truncate queue to QUEUE_CONTROL_LIMIT
+const int QUEUE_CONTROL_LIMIT = 2;
+// delay between iterations (in milliseconds)
+const int DELAY = 10;
+
+/*
+ * constants and variables for implementation of FIFO queue
+ * (see also README and functions and comments below)
+ */
+
+const int ARRAY_SIZE = 64;
 char *fifo[] = { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
 NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
@@ -26,21 +58,30 @@ int *p_head = &head;
 int *p_tail = &tail;
 int iterations = 0;
 int queue_length = 0;
-int status = 0;
-char *departure;
-char input_string[3] = "ab";
+
+// status of queueing system
+// 0: no error
+// 1: overflow
+int system_status = 0;
+
+// dummy input value, to be enqueued
+char arrival[3] = "ab";
+
+// pointer to dequeued value
+// NULL if queue was empty
+char *departure = NULL;
 
 /*
- * Function enqueue
- *   Add new item to queue
+ * function enqueue
+ *   add new item to queue
  *
- * Parameters:
+ * parameters:
  *   fifo:    array of strings (char pointers)
  *   size:    size of array
  *   p_tail:  pointer to index of tail
  *   arrival: string (char pointer) to be enqueued
  *
- * Return value:
+ * return value:
  *   0: no error
  *   1: overflow
  */
@@ -56,16 +97,16 @@ int enqueue(char *fifo[], int size, int *p_tail, char *arrival) {
 }
 
 /*
- * Function dequeue
- *   Remove item from queue, FIFO
+ * function dequeue
+ *   remove item from queue, FIFO
  *
- * Parameters:
+ * parameters:
  *   fifo:    array of strings (char pointers)
  *   size:    size of array
  *   p_head:  pointer to index of head
  *   p_tail:  pointer to index of tail
  *
- * Return value:
+ * return value:
  *   dequeued string (char pointer)
  *   NULL if queue was empty
  */
@@ -81,11 +122,14 @@ char* dequeue(char *fifo[], int size, int *p_head, int *p_tail) {
 }
 
 /*
- * Function get_queue_length
+ * function get_queue_length
  *
- * Parameters:
+ * parameters:
  *   fifo:       array of strings (char pointers)
  *   array_size: size of array
+ *
+ * return value:
+ *   queue length
  */
 int get_queue_length(char *fifo[], int array_size) {
 	int queue_length = 0;
@@ -98,11 +142,11 @@ int get_queue_length(char *fifo[], int array_size) {
 }
 
 /*
- * Function check_and_truncate
- *   If queue length exceeds the limit, truncate to this limit
+ * function check_and_truncate
+ *   if queue length exceeds the limit, truncate to this limit
  *   (current implementation: truncate starting from head of queue)
  *
- * Parameters:
+ * parameters:
  *   fifo:    array of strings (char pointers)
  *   size:    size of array
  *   p_head:  pointer to index of head
@@ -110,24 +154,20 @@ int get_queue_length(char *fifo[], int array_size) {
  */
 void check_and_truncate(char *fifo[], int size, int *p_head, int limit) {
 	int queue_length = get_queue_length(fifo, size);
-	while (fifo[*p_head] && queue_length > limit) {
+	while (fifo[*p_head] && (queue_length > limit)) {
 		fifo[*p_head] = NULL;
 		*p_head = (*p_head + 1) % size;
 		queue_length--;
 	}
 }
 
-void setup() {
-	// The MAX72XX is in power-saving mode on startup
-	lc.shutdown(0, false);
-	// Set the brightness to a medium value
-	lc.setIntensity(0, 8);
-	// clear the display
-	lc.clearDisplay(0);
-	// set seed for random number generator
-	srand(1234);
-}
-
+/*
+ * function write_led_matrix
+ *   visualize queue length on LED matrix
+ *
+ * parameters:
+ *   queue_length:  queue length
+ */
 void write_led_matrix(int queue_length) {
 	lc.clearDisplay(0);
 	int counter = 0;
@@ -141,29 +181,39 @@ void write_led_matrix(int queue_length) {
 	}
 }
 
+void setup() {
+	// the MAX72XX is in power-saving mode on startup
+	lc.shutdown(0, false);
+	// set the brightness to a medium value
+	lc.setIntensity(0, 8);
+	// clear the display
+	lc.clearDisplay(0);
+	// set seed for random number generator
+	srand(SEED);
+}
+
 void loop() {
-	if (status == 0) {
+	if (system_status == 0) {
 		iterations++;
-		// Enqueueing with probability 0.25, 0.30, 0.35
-		if (rand() % 100 < 35) {
-			status = enqueue(fifo, array_size, p_tail, input_string);
+		// enqueueing with probability ARRIVAL_PROB
+		if (rand() % 100 < ARRIVAL_PROB) {
+			system_status = enqueue(fifo, ARRAY_SIZE, p_tail, arrival);
 		}
-		// Dequeueing with probability 0.30
-		if (rand() % 100 < 30) {
-			departure = dequeue(fifo, array_size, p_head, p_tail);
+		// dequeueing with probability DEPARTURE_PROB
+		if (rand() % 100 < DEPARTURE_PROB) {
+			departure = dequeue(fifo, ARRAY_SIZE, p_head, p_tail);
 		}
-		// Control: truncate every 10 steps to 2 elements in queue
-		///*
-		if (iterations % 10 == 0) {
-			check_and_truncate(fifo, array_size, p_head, 2);
+		// control: truncate every QUEUE_CONTROL_INTERVAL steps to
+		// QUEUE_CONTROL_LIMIT elements in queue
+		if ((CONTROL == 'Y') && (iterations % QUEUE_CONTROL_INTERVAL == 0)) {
+			check_and_truncate(fifo, ARRAY_SIZE, p_head, QUEUE_CONTROL_LIMIT);
 			iterations = 0;
 		}
-		//*/
-		queue_length = get_queue_length(fifo, array_size);
+		queue_length = get_queue_length(fifo, ARRAY_SIZE);
 		write_led_matrix(queue_length);
 	} else {
-		// overflow
-		write_led_matrix(array_size);
+		// overflow - stop simulation
+		write_led_matrix(ARRAY_SIZE);
 	}
-	delay(10);
+	delay(DELAY);
 }
